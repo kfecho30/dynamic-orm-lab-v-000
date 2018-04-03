46d6691fc716d440bcbd29960f02991f5927124b
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 00:50:26 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    sql = "PRAGMA table_info ('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    col_names = []
+
+    table_info.each do |col|
+      col_names << col["name"]
+    end
+    col_names.compact
+  end
+
+  def initialize(options = {})
+  end
 end
