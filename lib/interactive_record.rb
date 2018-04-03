6df5678c1ee7fa1ddccf2aa419ecc5fbdccc58b2
Kelsey Fecho <kfecho30@gmail.com>
2018-04-03 01:04:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,26 +13,12 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
     sql = "PRAGMA table_info ('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     col_names = []
-
     table_info.each do |col|
       col_names << col["name"]
     end
     col_names.compact
-  end
-
-  def self.attr_accessor
-    self.column_names.each do |col|
-      attr_accessor col.to_sym
-    end
-  end
-
-  def initialize(options={})
-    options.each do |k, v|
-      self.send("#{k}=", v)
-    end
   end
 end
